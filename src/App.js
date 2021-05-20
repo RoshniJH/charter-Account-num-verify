58c645b62427085bdd8b23f48b0760324a14a8fa
RoshniJH <80098774+RoshniJH@users.noreply.github.com>
2021-05-20 18:10:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./index.css";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+function AppC() {
+  const [num,setNum] = useState('');
+  const options1 = [
+   'VANTAGE', 'OPTION 1', 'OPTION 2','OPTION 3'
+  ];
+  const defaultOption1 = options1[0];
+  const options2 = [
+    'CBR', 'OPTION 1', 'OPTION 2','OPTION 3'
+   ];
+   const defaultOption2 = options2[0];
+  const searchAlrt = (e) => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    e.preventDefault();
+    alert ("Searcing for account details  " + num)
+       
+  }
+    return (    
+      
+        <div className="app">
+        <div className="app__header">
+        <header>Charter Account Number Verification Portal</header>
+        </div>
+        <div className="app__form">
+        <form >
+        <label className="form__label">
+          Account Number:
+          <input 
+          className="acc__text" 
+          value={num}  
+          onChange={(e) =>setNum(e.target.value)} 
+          name="name"  />
+        </label>
+        <div className="app__drop">
+        <label>
+          System:
+        </label>
+        <Dropdown className="app__dropdown" options={options1}  value={defaultOption1} placeholder="Select an option" />
+        </div>
+        <div className="app__drop" >
+        <label>
+          Market:
+        </label>
+        <Dropdown className="app__dropdown"  options={options2}  value={defaultOption2} placeholder="Select an option" />
+        </div>
+        <button  onClick={searchAlrt} disabled={!num} type="submit">Search Account</button>
+      
+      </form>
+      
+        </div>
+        </div>
+    )
 }
 
-export default App;
+export default AppC
